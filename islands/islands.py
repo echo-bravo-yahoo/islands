bc8ccd48b0d12494fec988668cf6f09573b8a73a
Ashton Eby <ashton.eby@oracle.com>
2020-10-13 16:48:28 -0700

    First draft of sensor setup

---
+++ islands/islands.py
@@ -0,0 +1,72 @@
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import sched, time
+from weather import Weather
+import json
+import threading
+
+event_loop_group = io.EventLoopGroup(1)
+host_resolver = io.DefaultHostResolver(event_loop_group)
+client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+THING_NAME = "badge-and-printer"
+
+iot = mqtt_connection_builder.mtls_from_path(
+        endpoint="ayecs2a13r9pv-ats.iot.us-west-2.amazonaws.com",
+        cert_filepath="/home/swift/workspace/fbb3f88aee-certificate.pem.crt",
+        pri_key_filepath="/home/swift/workspace/fbb3f88aee-private.pem.key",
+        ca_filepath="/home/swift/workspace/AmazonRootCA1.pem",
+        client_id=THING_NAME,
+        client_bootstrap=client_bootstrap,
+        clean_session=False,
+        keep_alive_secs=6)
+
+scheduler = sched.scheduler(time.time, time.sleep)
+
+weather = Weather(iot, scheduler, virtual=True)
+
+print("Connecting to IOT.")
+iot.connect()
+print("Connected to IOT.")
+
+SHADOW_UPDATE_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update"
+SHADOW_UPDATE_ACCEPTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update/accepted"
+SHADOW_UPDATE_REJECTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update/rejected"
+SHADOW_UPDATE_DELTA_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update/delta"
+SHADOW_GET_TOPIC = "$aws/things/" + THING_NAME + "/shadow/get"
+SHADOW_GET_ACCEPTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/get/accepted"
+SHADOW_GET_REJECTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/get/rejected"
+
+def handle_delta(topic, payload, **kwargs):
+    print("Received new shadow delta.")
+    weather.handle_delta(payload)
+
+def handle_get_accepted(topic, payload, **kwargs):
+    print("Received shadow state.")
+    weather.handle_delta(payload)
+
+def handle_get_rejected(topic, payload, **kwargs):
+    print("---ERROR--- Fetching shadow state failed: " + json.dumps(json.loads(payload), sort_keys=True, indent=4))
+
+def handle_update_accepted(topic, payload, **kwargs):
+    print("Updated shadow state.")
+
+def handle_update_rejected(topic, payload, **kwargs):
+    print("---ERROR--- Updating shadow state failed: " + json.dumps(json.loads(payload), sort_keys=True, indent=4))
+
+print("Subscribing to shadow topics.")
+iot.subscribe(topic=SHADOW_UPDATE_DELTA_TOPIC, qos=mqtt.QoS.AT_LEAST_ONCE, callback=handle_delta)
+shadow_get_accepted_future, temp = iot.subscribe(topic=SHADOW_GET_ACCEPTED_TOPIC, qos=mqtt.QoS.AT_LEAST_ONCE, callback=handle_get_accepted)
+iot.subscribe(topic=SHADOW_GET_REJECTED_TOPIC, qos=mqtt.QoS.AT_LEAST_ONCE, callback=handle_get_rejected)
+iot.subscribe(topic=SHADOW_UPDATE_ACCEPTED_TOPIC, qos=mqtt.QoS.AT_LEAST_ONCE, callback=handle_update_accepted)
+iot.subscribe(topic=SHADOW_UPDATE_REJECTED_TOPIC, qos=mqtt.QoS.AT_LEAST_ONCE, callback=handle_update_rejected)
+
+print("Requesting new shadow state.")
+iot.publish(topic=SHADOW_GET_TOPIC, payload="", qos=mqtt.QoS.AT_LEAST_ONCE)
+print("Requested new shadow state.")
+
+sentinel = threading.Event()
+
+scheduler.run()
+
+sentinel.wait()
---
+++ islands/weather.py
@@ -0,0 +1,111 @@
+from awscrt import mqtt
+import json
+
+THING_NAME = "badge-and-printer"
+# this should be dynamic
+LOCATION = "den"
+MODULE_NAME = "weather"
+
+SHADOW_UPDATE_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update"
+SHADOW_UPDATE_ACCEPTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update/accepted"
+SHADOW_UPDATE_REJECTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update/rejected"
+SHADOW_UPDATE_DELTA_TOPIC = "$aws/things/" + THING_NAME + "/shadow/update/delta"
+SHADOW_GET_TOPIC = "$aws/things/" + THING_NAME + "/shadow/get"
+SHADOW_GET_ACCEPTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/get/accepted"
+SHADOW_GET_REJECTED_TOPIC = "$aws/things/" + THING_NAME + "/shadow/get/rejected"
+
+PUBLISH_TOPIC = "data/" + MODULE_NAME + "/" + LOCATION
+
+class Weather:
+    def __init__(self, iot, scheduler, virtual=False):
+        # Use virtual to test iot functionality on computers without busio / sensors.
+        if not virtual:
+            from busio import I2C
+            import adafruit_bme680
+            import board
+
+            # Create library object using our Bus I2C port
+            i2c = I2C(board.SCL, board.SDA)
+            self.bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+
+            # change this to match the location's pressure (hPa) at sea level
+            # this could be dynamically updated from, e.g.,
+            # https://forecast.weather.gov/MapClick.php?x=266&y=134&site=sew&zmx=&zmy=&map_x=266&map_y=134#.X2jtB2hKiUk
+            self.bme680.sea_level_pressure = 1013.89
+
+        self.iot = iot
+        self.scheduler = scheduler
+        self.virtual = virtual
+
+        # You will usually have to add an offset to account for the temperature of
+        # the sensor. This is usually around 5 degrees but varies by use. Use a
+        # separate temperature sensor to calibrate this one.
+        self.temperature_offset = -5
+
+        self.enabled = False
+
+        # Start the scheduled work
+        self.schedule()
+
+    def schedule(self):
+        self.publishResults()
+        self.scheduler.enter(60, 1, self.schedule)
+
+    def publishResults(self):
+        print("Publishing results to " + PUBLISH_TOPIC + ".")
+        if self.virtual:
+            print("Running in virtual mode; did not publish results to " + PUBLISH_TOPIC + ".")
+        else:
+            self.iot.publish(topic=PUBLISH_TOPIC, payload=self.generatePayload(), qos=mqtt.QoS.AT_LEAST_ONCE)
+
+
+    def generatePayload(self):
+        payload = {}
+        payload["temp"] = self.toFahrenheit(self.bme680.temperature + self.temperature_offset)
+        payload["gas"] = self.bme680.gas
+        payload["humidity"] = self.bme680.humidity
+        payload["pressure"] = self.bme680.pressure
+        payload["altitude"] = self.bme680.altitude
+        log(payload)
+        return json.dumps(payload)
+
+    def toFahrenheit(self, celsius):
+        return (celsius * 9/5) + 32
+
+    def log(self, payload):
+        print("\nTemperature: %0.1f F" % (payload["temp"]))
+        print("Gas: %d ohm" % payload["gas"])
+        print("Humidity: %0.1f %%" % payload["humidity"])
+        print("Pressure: %0.3f hPa" % payload["pressure"])
+        print("Altitude: %0.2f meters" % payload["altitude"])
+
+    def update_if_necessary(self, desired, reported):
+        if reported and desired["weather"].lower() != reported["weather"].lower():
+            payload = '{"state":{"reported":{"weather":"' + desired["weather"] + '"}}}'
+            print(payload)
+            self.iot.publish(topic=SHADOW_UPDATE_TOPIC, payload=payload, qos=mqtt.QoS.AT_LEAST_ONCE)
+
+
+    def handle_delta(self, payload):
+        print(json.dumps(json.loads(payload), sort_keys=True, indent=4))
+        try:
+            desired = json.loads(payload)["state"]["desired"]
+            reported = json.loads(payload)["state"]["reported"]
+        except KeyError:
+            desired = json.loads(payload)["state"]
+            reported = None
+
+        try:
+            if desired["weather"].lower() == "true":
+                print("Enabling weather module.")
+                self.enabled = True
+                self.update_if_necessary(desired, reported)
+            elif desired["weather"].lower() == "false":
+                print ("Disabling weather module.")
+                self.enabled = False
+                self.update_if_necessary(desired, reported)
+            else:
+                raise ValueError("Weather should be a stringified boolean.")
+        except KeyError:
+            pass
+
