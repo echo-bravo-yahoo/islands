0f0fa3b64f96dd7bee6fb65290c9db4dde62909d
Ashton Eby <ashton.eby@oracle.com>
2020-11-27 16:39:59 -0800

    Fix tab spacing

--- islands/weather.py
+++ islands/weather.py
@@ -53,8 +53,8 @@ class Weather:
         self.schedule()
 
     def disable(self):
-	if self.bme680:
-	    del self.bme680
+        if self.bme680:
+          del self.bme680
         self.scheduler.cancel(self.scheduledEvent)
 
     def publishResults(self):
