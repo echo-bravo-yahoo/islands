306119e07a5f6f81b54ebc5a07ddb865ff834aa3
Ashton Eby <ashton.eby@oracle.com>
2021-02-16 22:08:00 -0800

    Add test frame

--- bitbang/index.js
+++ bitbang/index.js
@@ -53,26 +53,30 @@ async function numberToActions(number, width) {
   }
 }
 
+async function messageToActions(message) {
+  await io.setup(16, io.DIR_OUT)
 
-/*
-console.log(numberToBitString(0x11, 8))
-numberToActions(0x11, 8).then(() => {
+  for (let index = 0; index < message.length; index++) {
+    await numberToActions(message, 8)
+  }
   console.log('LED ON for', 500, 'microseconds')
+  await io.write(16, true)
   sleep(500)
+  await io.write(16, false)
   console.log('DONE')
-})
-*/
+}
 
 (async() => {
-  await io.setup(16, io.DIR_OUT)
-  await numberToActions(0x11, 8).then(async () => {
-    console.log('LED ON for', 500, 'microseconds')
-    await io.write(16, true)
-    sleep(500)
-    await io.write(16, false)
-    console.log('DONE')
-  })
+  await messageToActions([
+    0x11, 0xda, 0x27, 0x00, 0x00, 0x49, 0x2C, 0x00, 0x5F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x66
+  ])
 })()
+
+// "build a message from scratch"
+// |   header    | Msg Id | Mode | Temp | Fixed | Fan | Fixed |  Timers  | Pwrful | Fixed | Econo | Fixed | Checksum |
+// | 11 da 27 00 |   00   |  xx  |  xx  |   00  |  xx |   00  | xx xx xx |   0x   |   00  |   8x  |   00  |    xx    |
+// "heat" at 72F, 3/5 fan speed, swing enabled
+// | 11 da 27 00 |   00   |  49  |  2C  |   00  |  5F |   00  | 00 00 00 |   00   |   00  |   80  |   00  |    66    |
 
 // manual: https://www.daikinac.com/content/assets/DOC/OperationManuals/01-EN-3P379751-4C.pdf
 // comfort mode: blows up for cool, down for heat 'avoid blowing on you'
