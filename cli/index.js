eed5dca45cefa8657d6264fd73542890aaf74870
Ashton Eby <ashton.eby@oracle.com>
2022-04-08 10:16:46 -0700

    (WIP) try to get ssh working...

--- cli/index.js
+++ cli/index.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { readFileSync } from 'fs'
+import { spawn, spawnSync, execSync } from 'child_process'
+import { normalize, join } from 'path'
+import { Client } from 'ssh2';
 
 const islands = [
   {
     "name": "skidaway",
-    "ip": "129.168.1.127", // is this right?
-    "location": "home office",
-    "attached": ["thermal-printer"]
+    "ip": "129.168.1.127",
+    "location": "den",
+    "locationHuman": "home office",
+    "attached": ["bme680", "thermal-printer"],
+    "user": "pi",
+    "certLocation": "/home/swift/.ssh/diag-instance.key"
+  },
+  {
+    "name": "fiji",
+    "ip": "129.168.1.126",
+    "location": "porch",
+    "locationHuman": "outside porch",
+    "attached": [],
+    "user": "pi",
+    "certLocation": "/home/swift/.ssh/diag-instance.key"
   }
 ]
 
@@ -30,6 +46,64 @@ yargs(hideBin(process.argv))
       }
 
       islands.forEach((island) => console.log(formatIsland(island)))
+    }
+  })
+  .command({
+    command: 'ssh <island>',
+    description: 'ssh to an island',
+    builder: yargs => {
+      return yargs
+    },
+    handler: args => {
+      const destination = islands.find((island) => island.name === args.island)
+      const config = {
+          host: destination.ip,
+          port: 22,
+          username: destination.user
+      }
+
+      if (destination.password) {
+        config.password = destination.password
+      }
+
+      if (destination) {
+        console.log('sshing...')
+        /*
+        console.log(execSync('whoami').toString())
+        process.stdin.setRawMode(false)
+        let a = spawn(`ssh`, ['-l pi', `-i ${destination.certLocation}`, `pi@${destination.ip}`, '-T', '-o StrictHostKeyChecking=no'], {
+          stdio: [process.stdin, process.stdout, process.stderr],
+          shell: '/bin/zsh'
+        })
+        a.on('connection', () => { console.log('connected') })
+        a.on('data', () => { console.log('data') })
+        a.on('error', () => { console.log('error') })
+        const { readFileSync } = require('fs');
+        */
+
+        const conn = new Client();
+        conn.on('ready', () => {
+          console.log('Client :: ready');
+          conn.shell((err, stream) => {
+            if (err) throw err;
+            stream.on('close', () => {
+              console.log('Stream :: close');
+              conn.end();
+            }).on('data', (data) => {
+              console.log('OUTPUT: ' + data);
+            });
+            stream.end('ls -l\nexit\n');
+          });
+        }).connect({
+          host: destination.ip,
+          port: 22,
+          username: 'pi',
+          privateKey: readFileSync(destination.certLocation),
+          debug: console.log
+        });
+      } else {
+        console.error(`could not find island ${args.island}`)
+      }
     }
   })
   .help()
