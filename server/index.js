57f3c3c9327fde580aa7649c10ff483ba4a46d33
Ashton Eby <ashton.eby@oracle.com>
2022-05-27 15:06:18 -0700

    (feat) first draft of server

---
+++ server/index.js
@@ -0,0 +1,98 @@
+(async () => {
+  // libraries
+  const express = require('express')
+  const { IoTDataPlaneClient, PublishCommand } = require("@aws-sdk/client-iot-data-plane")
+  // import { IoTDataPlaneClient, PublishCommand } from "@aws-sdk/client-iot-data-plane"
+  const path = require('path')
+  const { readFileSync } = require('fs')
+  const AWSXRay = require('aws-xray-sdk')
+
+
+  // config and constructed objects
+  const config = require('./config.json')
+  const client = new IoTDataPlaneClient(config)
+  const app = module.exports = express()
+
+  // behavior flags
+  const trace = false
+
+  if (trace) app.use(AWSXRay.express.openSegment('MyApp'))
+
+  app.get('/app', function(req, res) {
+    res.status(200).sendFile(path.join(__dirname, './app.html'))
+  })
+
+  app.get('/thermostat/:desired', async function(req, res) {
+    let input = {
+      powerful: true,
+      econo: false,
+      fanMode: 'AUTO'
+    }
+
+    if (req.params.desired === 'coldest') {
+      input = {
+        mode: 'COLD',
+        temp: 68,
+        fanSwing: true,
+        comfort: false
+      }
+    } else if (req.params.desired === 'cold') {
+      input = {
+        mode: 'COLD',
+        temp: 70,
+        fanSwing: true,
+        comfort: false
+      }
+    } else if (req.params.desired === 'hottest') {
+      input = {
+        mode: 'HEAT',
+        temp: 75,
+        fanSwing: false,
+        comfort: false
+      }
+    } else if (req.params.desired === 'hot') {
+      input = {
+        mode: 'HEAT',
+        temp: 72,
+        fanSwing: false,
+        comfort: true
+      }
+    } else if (req.params.desired === 'fanOnly') {
+      input = {
+        mode: 'FAN',
+        // temp doesn't matter, but has to be populated
+        temp: 70,
+        fanSwing: true,
+        comfort: true
+      }
+    } else if (req.params.desired === 'auto') {
+      input = {
+        mode: 'AUTO',
+        temp: 72,
+        fanSwing: true,
+        comfort: true
+      }
+    } else if (req.params.desired === 'off') {
+      input = {
+        mode: 'OFF',
+        temp: 72,
+        fanSwing: false,
+        comfort: true
+      }
+    } else {
+      res.status(400).send('Bad input')
+    }
+
+    const command = new PublishCommand({ topic: '$aws/things/fiji/shadow/update', payload: JSON.stringify({ state: { desired: { airConditioning: input } } }) })
+    const response = await client.send(command)
+    res.status(200).send('Hello World:\n' + JSON.stringify(response, null, 4))
+  })
+
+  /* istanbul ignore next */
+  if (!module.parent) {
+    app.listen(3009, '0.0.0.0')
+    console.log('Express started on port 3009')
+  }
+
+  if (trace) app.use(AWSXRay.express.closeSegment())
+})()
