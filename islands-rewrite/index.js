121908fbf5fd2257122c08cef3de3431840e06f2
Ashton Eby <ashton.eby@oracle.com>
2022-09-05 17:01:29 -0700

    add node rewrite

---
+++ islands-rewrite/index.js
@@ -0,0 +1,46 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+// docs: https://aws.github.io/aws-iot-device-sdk-js-v2/node/index.html
+import { mqtt, iot } from 'aws-iot-device-sdk-v2'
+
+const config = require("./config.json");
+
+function build_connection() {
+    let config_builder =
+      iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(config.certFilePath, config.privateKeyFilePath)
+
+    config_builder.with_certificate_authority_from_path(undefined, config.awsCertFilePath)
+    config_builder.with_clean_session(false)
+    config_builder.with_client_id(config.name)
+    config_builder.with_endpoint(config.endpoint)
+    const clientConfig = config_builder.build()
+    const client = new mqtt.MqttClient()
+    return client.new_connection(clientConfig)
+}
+
+async function onPublish(topic, payload, dup, qos, retain) {
+    var decoder = new TextDecoder("utf-8");
+    const json = JSON.parse(decoder.decode(payload))
+    console.log(`Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`);
+    console.log(JSON.stringify(json, null, 2))
+}
+
+// this non-resolved promise keeps the process running
+const shouldRun = new Promise(() => {})
+
+console.log("Connecting...")
+const connection = build_connection()
+console.log(await connection.connect())
+console.log("Connection completed.")
+console.log("Subscribing...")
+console.log(await connection.subscribe("test", mqtt.QoS.AtLeastOnce, onPublish))
+console.log("Subscribe completed.")
+console.log("Publishing...")
+console.log(await connection.publish("test", { test: "test" }, mqtt.QoS.AtLeastOnce))
+console.log("Publish completed.")
+
+// TODO: clean up on sigkill, etc
+// console.log("Disconnecting...")
+// console.log(await connection.disconnect())
+// console.log("Disconnecting completed.")
