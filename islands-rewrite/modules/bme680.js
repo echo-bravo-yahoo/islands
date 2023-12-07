9bb8ce4996d695fe2150b45ca6d3388a32fc09db
Ashton Eby <ashton.eby@oracle.com>
2023-12-06 19:02:23 -0800

    (chore) continue refactoring

--- islands-rewrite/modules/bme680.js
+++ islands-rewrite/modules/bme680.js
@@ -9,23 +9,24 @@ import { Module } from './generic-module.js'
 
 let interval
 
-async function enable() {
-  globals.logger.info({ role: 'breadcrumb' }, `Enabling bme680...`)
+async function enable(desiredState) {
+  globals.logger.info({ role: 'breadcrumb' }, `Enabling bme680 to publish every ${desired.interval || 60} seconds...`)
   await sensor.initialize()
+  if (interval) clearInterval(interval)
   interval = setInterval(async() => {
     const sensorData = await sensor.getSensorData()
     const payload = {
-      temp: (sensorData.data.temperature) * 1.8 + 32,
-      gas: sensorData.data.gas_resistance,
-      humidity: sensorData.data.humidity,
-      pressure: sensorData.data.pressure
+      temp: ((sensorData.data.temperature) * 1.8 + 32) + (desiredState.offsets.temp || 0),
+      gas: sensorData.data.gas_resistance + (desiredState.offsets.gas || 0),
+      humidity: sensorData.data.humidity + (desiredState.offsets.humidity || 0),
+      pressure: sensorData.data.pressure + (desiredState.offsets.pressure || 0)
       // TODO: is this worth implementing?
       // altitude:
     }
     globals.logger.info({ role: 'breadcrumb' }, 'Publishing new bme680 data.')
-    globals.logger.info({ role: 'blob', blob: payload }, `bme680 data, published to data/weather/${globals.island.location || 'unknown'}: ${JSON.stringify(payload)}`)
+    globals.logger.info({ role: 'blob', blob: { payload, offsets: desiredState.offsets } }, `bme680 data, published to data/weather/${globals.island.location || 'unknown'}: ${JSON.stringify(payload)}`)
     globals.connection.publish(`data/weather/${globals.island.location || 'unknown'}`, payload, mqtt.QoS.AtLeastOnce)
-  }, 60*1000)
+  }, desiredState.interval * 1000 || 60 * 1000)
   globals.logger.info({ role: 'breadcrumb' }, `Enabled bme680.`)
 }
 
