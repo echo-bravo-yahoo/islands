2b482241feb4562114c6db5ad1090d6bc794b566
Ashton Eby <ashton.eby@oracle.com>
2023-12-12 10:46:12 -0800

    (draft) first draft of provisioning script

---
+++ provisioner/README.md
@@ -0,0 +1,3 @@
+We're using [sdm](https://github.com/gitbls/sdm) to provision islands.
+
+Image from [here](https://downloads.raspberrypi.org//raspios_lite_arm64/images/raspios_lite_arm64-2023-12-11/).
---
+++ provisioner/provision.mjs
@@ -0,0 +1,26 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const config = require('./config.json')
+
+const img = '2023-12-11-raspios-bookworm-arm64-lite.img'
+
+let customize = 'sudo sdm --customize '
+customize += `--plugin user:"setpassword=pi|password=${config.password}" `
+customize += `--plugin L10n:host `
+customize += `--plugin disables:piwiz `
+customize += `--plugin network:"wifissid=${config.wifi.ssid}|wifipassword=${config.wifi.password}" `
+customize += `--plugin copydir:"from=${config.islands.srcPath}|to=${config.islands.destPath}" `
+customize += `--regen-ssh-host-keys `
+customize += `--restart `
+customize += `${img}`
+
+console.log(customize)
+
+const device = '/dev/sde '
+
+let burn = `sudo sdm --burn ${device} `
+burn += `--hostname ${config.hostname} `
+burn += `--expand-root `
+burn += `${img}`
+
+console.log(burn)
