b63258f8f402bf47da2435c7d6cdea55362cec2f
Ashton Eby <ashton.eby@oracle.com>
2023-12-14 12:06:54 -0800

    (draft) install git, cmake

--- provisioner/provision.mjs
+++ provisioner/provision.mjs
@@ -10,13 +10,14 @@ customize += `--plugin L10n:host `
 customize += `--plugin disables:piwiz `
 customize += `--plugin network:"wifissid=${config.wifi.ssid}|wifipassword=${config.wifi.password}" `
 customize += `--plugin copydir:"from=${config.islands.srcPath}|to=${config.islands.destPath}" `
+customize += `--plugin apps:"apps=git,cmake`
 customize += `--regen-ssh-host-keys `
 customize += `--restart `
 customize += `${img}`
 
 console.log(customize)
 
-const device = '/dev/sde '
+const device = '/dev/sde'
 
 let burn = `sudo sdm --burn ${device} `
 burn += `--hostname ${config.hostname} `
