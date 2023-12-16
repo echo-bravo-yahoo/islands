7507240cf33afb8fa12452eb82b837f4bbb3e709
Ashton Eby <ashton.eby@oracle.com>
2023-12-15 23:05:22 -0800

    (draft) add provisioning steps to allow building the aws-iot-device-sdk

--- provisioner/provision.mjs
+++ provisioner/provision.mjs
@@ -10,7 +10,16 @@ customize += `--plugin L10n:host `
 customize += `--plugin disables:piwiz `
 customize += `--plugin network:"wifissid=${config.wifi.ssid}|wifipassword=${config.wifi.password}" `
 customize += `--plugin copydir:"from=${config.islands.srcPath}|to=${config.islands.destPath}" `
-customize += `--plugin apps:"apps=git,cmake,golang`
+
+// cmake and golang are required to build aws-crt
+customize += `--plugin apps:"apps=git,cmake,golang" `
+
+// default swap is 100 mb, and is too small to compile aws-crt (dependency of aws-iot-device-sdk-v2)
+// have not dialed it in yet, but 4 GB does provide enough headroom to build aws-crt
+customize += `--plugin system:"swap=4096" `
+
+// customize += `--plugin raspiconfig:"overclock=`
+
 customize += `--regen-ssh-host-keys `
 customize += `--restart `
 customize += `${img}`
