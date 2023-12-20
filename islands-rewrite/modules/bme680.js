22b2ea4d8968c90d334de184ab42f01123228721
Ashton Eby <ashton.eby@oracle.com>
2023-12-20 15:06:34 -0800

    (draft) add nodejs native bme280 implementation

--- islands-rewrite/modules/bme680.js
+++ islands-rewrite/modules/bme680.js
@@ -7,30 +7,33 @@ const sensor = new Bme680(1, 0x77)
 import { globals } from '../index.js'
 import { Module } from './generic-module.js'
 
-let interval
+let interval, sensor
+
+async function publichReading() {
+  const sensorData = await sensor.getSensorData()
+  const payload = {
+    temp: (sensorData.data.temperature) * 1.8 + 32,
+    gas: sensorData.data.gas_resistance,
+    humidity: sensorData.data.humidity,
+    pressure: sensorData.data.pressure
+    // TODO: is this worth implementing?
+    // altitude:
+  }
+  globals.logger.info({ role: 'breadcrumb' }, 'Publishing new bme680 data.')
+  globals.logger.info({ role: 'blob', blob: payload }, `bme680 data, published to data/weather/${globals.island.location || 'unknown'}: ${JSON.stringify(payload)}`)
+  globals.connection.publish(`data/weather/${globals.island.location || 'unknown'}`, payload, mqtt.QoS.AtLeastOnce)
+}
 
 async function enable() {
   globals.logger.info({ role: 'breadcrumb' }, `Enabling bme680...`)
   await sensor.initialize()
-  interval = setInterval(async() => {
-    const sensorData = await sensor.getSensorData()
-    const payload = {
-      temp: (sensorData.data.temperature) * 1.8 + 32,
-      gas: sensorData.data.gas_resistance,
-      humidity: sensorData.data.humidity,
-      pressure: sensorData.data.pressure
-      // TODO: is this worth implementing?
-      // altitude:
-    }
-    globals.logger.info({ role: 'breadcrumb' }, 'Publishing new bme680 data.')
-    globals.logger.info({ role: 'blob', blob: payload }, `bme680 data, published to data/weather/${globals.island.location || 'unknown'}: ${JSON.stringify(payload)}`)
-    globals.connection.publish(`data/weather/${globals.island.location || 'unknown'}`, payload, mqtt.QoS.AtLeastOnce)
-  }, 60*1000)
+  interval = setInterval(publishReading, 60*1000)
   globals.logger.info({ role: 'breadcrumb' }, `Enabled bme680.`)
 }
 
 async function disable() {
   globals.logger.info({ role: 'breadcrumb' }, `Disabling bme680...`)
+  // TODO: do I need to turn off the sensor / close the connection?
   clearInterval(interval)
   globals.logger.info({ role: 'breadcrumb' }, `Disabled bme680.`)
 }
