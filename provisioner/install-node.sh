b45cfba773b0528454cb9ef2c511cd1bb38d008f
Ashton Eby <ashton.eby@oracle.com>
2023-12-26 15:16:22 -0800

    (draft) setup pm2 while provisioning

--- provisioner/install-node.sh
+++ provisioner/install-node.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+# set up node and npm
 # run as root or sudo
-
 chown -R pi:pi /home/pi/islands && \
 cd /usr/bin && \
 ln -s /usr/local/node/bin/node node && \
-ln -s /usr/local/node/bin/npm npm
+ln -s /usr/local/node/bin/npm npm;
+
+# set up PATH
+printf '\n\nexport PATH=$PATH:/usr/local/node/bin' >> /home/pi/.bashrc
+
+# set up pm2
+npm install pm2 --location=global && \
+sudo env PATH=$PATH:/usr/local/node/bin /usr/local/node/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi && \
+pm2 start /home/pi/islands/islands-rewrite/island.pm2.config.cjs && \
+pm2 save
