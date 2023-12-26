ec2355221c59a6cafe8aa5c737c27b14113dee0a
ashton eby <ashton.eby@gmail.com>
2023-12-26 14:54:44 -0800

    (draft) fix require issue with pm2 conf

--- islands-rewrite/island.pm2.config.cjs
+++ islands-rewrite/island.pm2.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "island",
-    script : "./islands-rewrite/index.js",
+    script : "./index.js",
     watch: ["."],
     ignore_watch: ["./node_modules", "../.git"],
     max_restarts: 3,
