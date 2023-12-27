090ce195167e986a18806d694c94569218102568
Ashton Eby <ashton.eby@oracle.com>
2023-12-26 18:08:48 -0800

    (draft) bugfix

--- islands-rewrite/island.pm2.config.cjs
+++ islands-rewrite/island.pm2.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name   : "island",
     script : "/home/pi/islands/islands-rewrite/index.js",
     watch: ["/home/pi/islands"],
-    ignore_watch: ["/home/pi/islands/node_modules", "/home/pi/islands/.git"],
+    ignore_watch: [".*node_modules.*", ".*\.git"],
     max_restarts: 3,
     min_uptime: 10000
   }]
