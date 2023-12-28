4afd3e298f3b724216b324e49b982bd72c0d12a5
Ashton Eby <ashton.eby@oracle.com>
2023-12-28 12:12:57 -0800

    (draft) add logging to mqttEmitter fn

--- lambdas/mqttEmitter/index.js
+++ lambdas/mqttEmitter/index.js
@@ -8,11 +8,15 @@ exports.handler = async (event, context, callback) => {
     qos: 1
   }
 
+  console.log(`Re-broadcasting event ${JSON.stringify(JSON.parse(params.payload), null, 2)} for topic ${params.topic}.`)
+
   const promise = new Promise((resolve, reject) => {
     iot.publish(params, function(err, data) {
       if (err) {
+        console.log(err)
         reject(err)
       } else {
+        console.log('Event successfully re-broadcast.')
         resolve(event)
       }
     })
