902dc7df7be869aa3cd583af6d90245b7f9ed4bc
Ashton Eby <ashton.eby@gmail.com>
2024-07-06 12:36:31 -0700

    (bug) prevent pm2 from watching any .git dirs

--- islands-rewrite/island.pm2.config.cjs
+++ islands-rewrite/island.pm2.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name   : "island",
     script : "/home/pi/islands/islands-rewrite/index.js",
     watch: ["/home/pi/islands/islands-rewrite"],
-    ignore_watch: ["**/node_modules/**", ".git/**", "**/*.log"],
+    ignore_watch: ["**/node_modules/**", "**/.git/**", "**/*.log"],
     max_restarts: 3,
     min_uptime: 10000
   }]
