fe4a4413a5111f872cc610950c429f91edd9dafc
Ashton Eby <ashton.eby@gmail.com>
2024-11-18 22:41:00 -0800

    update metric handlers for influxdb

--- lambdas/metricsHandler/index.js
+++ lambdas/metricsHandler/index.js
@@ -1,12 +1,67 @@
 #!/usr/bin/env node
 
 import { CloudWatchClient, PutMetricDataCommand } from '@aws-sdk/client-cloudwatch'
+import { exec } from 'node:child_process'
+/*
+{
+    "readings": {
+        "metadata": {
+            "island": "skeppsholmen",
+            "timestamp": "2024-11-19T04:59:43.405Z"
+        },
+        "temp": 76.52394317895174,
+        "humidity": 28.606149841897132,
+        "pressure": 1003.3289466281659
+    },
+    "dimensions": {
+        "island": "skeppsholmen",
+        "location": "office"
+    }
+}
+*/
 
 export async function handler(event, context, callback) {
-  const client = new CloudWatchClient({ region: "us-west-2" })
   console.log(`event: ${JSON.stringify(event, null, 2)}`)
 
+  await Promise.all([
+    logToCloudwatch(event),
+    logToInflux(event),
+  ])
+}
+
+async function logToInflux(event) {
+  let data = [],
+    labels = []
+
+  for (const [key, value] of Object.entries(event.readings)) {
+    if (key !== 'metadata') {
+      data.push(`${key}=${value}`)
+    }
+  }
+
+  for (const [labelKey, labelValue] of Object.entries(event.dimensions)) {
+    labels.push(`${labelKey}=${labelValue}`)
+  }
+
+  labels = labels.join(',')
+  if (labels.length) labels = `,${labels}`
+  data = data.join(',')
+
+  let line = `weather${labels} ${data} ${new Date().valueOf()}`
+  let url = `http://echobravoyahoo.net:8086/api/v2/write?org=echo-bravo-yahoo&bucket=islands&precision=ms`
+  let command = `curl --request POST \
+                 --header "Authorization: Token ${process.env.influxdb_token}" \
+                 --header "Content-Type: text/plain; charset=utf-8" \
+                 --header "Accept: application/json" \
+                 --data-binary "${line}" \
+                "${url}"`
+    console.log(`Running command:\n${command}`)
+  return exec(command)
+}
+
+async function logToCloudwatch(event) {
   const data = []
+  const client = new CloudWatchClient({ region: "us-west-2" })
   const dimensions = Object.entries(event.dimensions).map((entry) => {
     return { Name: entry[0], Value: entry[1] }
   })
@@ -31,9 +86,7 @@ export async function handler(event, context, callback) {
 
 
   const command = new PutMetricDataCommand(params)
-  const response = await client.send(command)
-
-  return response
+  return client.send(command)
 }
 
 // if we aren't in a lambda function (that is, we're running locally):
